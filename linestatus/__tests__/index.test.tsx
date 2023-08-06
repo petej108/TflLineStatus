11fef9f1a50a2fdaaf99c6b0f0d7df9a4d55d0b6
Peter Jones <PeterJones@tfl.gov.uk>
2023-08-06 18:06:06 +0100

    updated test

--- linestatus/__tests__/index.test.tsx
+++ linestatus/__tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/pages/index';
 
-describe('Home', () => {
+describe('Line Status Home', () => {
   it('renders a heading', () => {
     render(<Home />);
 
