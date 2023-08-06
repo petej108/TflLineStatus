d74763c1bc12da88b38b1737a136075964ea053c
Peter Jones <PeterJones@tfl.gov.uk>
2023-08-06 14:38:10 +0100

    removed unused code

--- linestatus/components/LineInfoList.tsx
+++ linestatus/components/LineInfoList.tsx
@@ -2,13 +2,6 @@ import LineInfo from './LineInfo';
 import { lineInfoType } from 'types/lineInfo';
 import mockData from '../data/lineStatusMock.json';
 
-interface IlineInfo {
-  lineName: string;
-  lineStatus: string;
-}
-
-//const data = mockData as lineInfoType[];
-
 const LineInfoList = () => {
   const getLineInfo = (lineInfoRaw: any): lineInfoType => {
     const lineInfo: lineInfoType = {
@@ -19,11 +12,11 @@ const LineInfoList = () => {
     return lineInfo;
   };
 
-  const LineInfoList2: lineInfoType[] = mockData.map(getLineInfo);
+  const LineInfoListData: lineInfoType[] = mockData.map(getLineInfo);
 
   return (
     <>
-      {LineInfoList2.map((lineInfo) => (
+      {LineInfoListData.map((lineInfo) => (
         <LineInfo {...lineInfo} />
       ))}
     </>
