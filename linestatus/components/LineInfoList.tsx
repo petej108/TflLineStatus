b1db01a3d5764f92bb7d80d74705595564182a62
Peter Jones <PeterJones@tfl.gov.uk>
2023-08-06 14:36:08 +0100

    removed hard coded rows

--- linestatus/components/LineInfoList.tsx
+++ linestatus/components/LineInfoList.tsx
@@ -10,23 +10,6 @@ interface IlineInfo {
 //const data = mockData as lineInfoType[];
 
 const LineInfoList = () => {
-  const lineInfoData: lineInfoType = {
-    lineName: 'circle line',
-    lineStatus: 'all is good!',
-  };
-  const lineInfoData2: lineInfoType = {
-    lineName: 'district line',
-    lineStatus: 'all is good!',
-  };
-  const lineInfoData3: lineInfoType = {
-    lineName: 'elizabeth line',
-    lineStatus: 'all is good!',
-  };
-  const lineInfoData4: lineInfoType = {
-    lineName: 'hammersmith and city line',
-    lineStatus: 'all is good!',
-  };
-
   const getLineInfo = (lineInfoRaw: any): lineInfoType => {
     const lineInfo: lineInfoType = {
       lineName: lineInfoRaw.name as string,
@@ -43,10 +26,6 @@ const LineInfoList = () => {
       {LineInfoList2.map((lineInfo) => (
         <LineInfo {...lineInfo} />
       ))}
-      <LineInfo {...lineInfoData} />
-      <LineInfo {...lineInfoData2} />
-      <LineInfo {...lineInfoData3} />
-      <LineInfo {...lineInfoData4} />
     </>
   );
 };
