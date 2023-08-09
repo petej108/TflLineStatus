b2654f43919429e0ee410ceb295b3a13a92f7e65
Peter Jones <PeterJones@tfl.gov.uk>
2023-08-09 13:15:55 +0100

    added mock for data call

--- linestatus/__tests__/index.test.tsx
+++ linestatus/__tests__/index.test.tsx
@@ -1,5 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/pages/index';
+import lineInfoApiDataMock from '../data/lineStatusMock.json';
+
+jest.mock('../components/useLineStatusRequest', () => ({
+  useGetLineStatus: () => ({
+    data: lineInfoApiDataMock,
+    error: null,
+  }),
+}));
 
 describe('Line Status Home', () => {
   it('renders a heading', () => {
@@ -11,4 +19,6 @@ describe('Line Status Home', () => {
 
     expect(heading).toBeInTheDocument();
   });
+
+  
 });
