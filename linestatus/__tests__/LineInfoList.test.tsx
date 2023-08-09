e2f277019d68c087f3c2ea5816d53e4083d75487
Peter Jones <PeterJones@tfl.gov.uk>
2023-08-09 09:36:37 +0100

    updated tests. added readme

---
+++ linestatus/__tests__/LineInfoList.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import LineInfoList from '@/components/LineInfoList';
+import lineInfoApiDataMock from '../data/lineStatusMock.json';
+
+describe('Line Status List', () => {
+  it('renders Line Info List unchanged', () => {
+    const { container } = render(
+      <LineInfoList linesData={lineInfoApiDataMock} />
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Line info List contains all items', () => {
+    const { container } = render(
+      <LineInfoList linesData={lineInfoApiDataMock} />
+    );
+ 
+
+    expect(container.getElementsByClassName('card').length).toBe(lineInfoApiDataMock.default.length)
+    
+  });
+});
