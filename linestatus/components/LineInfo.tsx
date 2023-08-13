b8e0e72a326589403232fa0ccdc1b3b25d3c3099
Peter Jones <PeterJones@tfl.gov.uk>
2023-08-13 13:36:25 +0100

    removed tabindex

--- linestatus/components/LineInfo.tsx
+++ linestatus/components/LineInfo.tsx
@@ -3,10 +3,7 @@ import styles from '@/pages/index.module.css';
 
 const LineInfo = ({ lineName, lineStatus, lineId }: lineInfoType) => {
   return (
-    <li
-      id={lineId}
-      tabIndex={0}
-      className={[styles.card, styles.grid].join(' ')}>
+    <li id={lineId} className={[styles.card, styles.grid].join(' ')}>
       <div className={[lineId, styles.linecolor].join(' ')}></div>
       <div>{lineName}</div> <div>{lineStatus}</div>
     </li>
